58e1384c01ca8a45191f91d30ed2ba457797ea67
alexandre <aluq93@gmail.com>
2019-04-11 21:54:37 +0200

    first commit

---
+++ src/footer.php
@@ -0,0 +1,8 @@
+<footer>
+	<div class="container">
+		<p>Des questions ? Appelez le 0800 745 623</p>
+		<a href="#">Conditions des cartes cadeaux</a>
+		<a href="#">Conditions d'utilisation</a>
+		<a href="#">Déclaration de confidentialité</a>
+	</div>
+</footer>
